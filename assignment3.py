b7d247e6fbd97ae3776619e9e2ba0ef903d6014d
mortazakiyani <36876384+mortazakiyani@users.noreply.github.com>
2018-03-28 14:32:23 +0430

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -9,6 +9,20 @@ host_names=host_names.split('\n')
 for x in host_names:
     for y in ports:
         try:
-           # you should write your codes in this block
+           p = socket.socket()
+            p.connect((x, y))
+            answer = s.recv(1024)
+            p.close()
+            print(answer)
+            if "FreeFloat Ftp Server (Version 1.00)" in answer:
+                print("[+] FreeFloat server is vulnerable ", '\n');
+            elif "3Com 3CDaemon FTP Server Version 2.0" in answer:
+                print("[+] 3CDaemon server is vulnerable ", '\n');
+            elif "Ability Server 2.34" in answer:
+                print("[+] Ability server is vulnerable ", '\n');
+            elif "Sami FTP Server 2.0.2" in answer:
+                print("[+] Sami server is vulnerable ", '\n');
+            else:
+                print("[-] FTP server is not vulnerable", '\n');
         except Exception as e:
             print(e)
